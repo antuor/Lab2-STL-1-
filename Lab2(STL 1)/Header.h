2cdbff7ca83cb901f803a4e0599b21d8b8b4085e
Anton <antourv@gmail.com>
2018-10-04 21:43:33 +0700

    Добавьте файлы проекта.

---
+++ Lab2(STL 1)/Header.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <iterator>
+#include <fstream>
+#include <string>
+#include <set>
+#include <type_traits>
+
+const unsigned int min = 10, max = 50; // использование констант для большей понятливости
+
+// Структура сравнения
+
+bool compare (std::string const& a, std::string const& b)
+{
+	return a.size() < b.size();
+}
+
+void CompareOfString() 
+{
+	using Compare = std::integral_constant<decltype(&compare), &compare>; // использование сравнениня
+	std::multiset <std::string, Compare> stroka; // создание multiset
+	try // обработка ошибки выделения памяти
+	{
+		std::ifstream fin("C://Users/andbo/source/repos/Lab2(STL 1)/Debug/input.txt"); // открытия файла для чтения
+		if (!fin.is_open()) // если файл не открыт
+			std::cout << "Файл не может быть открыт!\n"; // сообщить об этом
+		else
+		{
+			std::string symb;
+			while (std::getline(fin, symb)) // читаем строки до конца файла
+			{
+				if (symb.size() > min && symb.size() < max)
+				{
+					stroka.insert(symb); // добавляем строку в multiset
+				}
+			}
+			fin.close(); // закрываем файл
+			std::ofstream fout("C://Users/andbo/source/repos/Lab2(STL 1)/Debug/output.txt"); // открываем файл для записи
+			std::multiset <std::string> ::iterator it = stroka.begin(); // созздаем итератор
+			for (it = stroka.begin(); it != stroka.end(); it++)
+			{
+				fout << *it << std::endl; // выводим в файл
+			}
+			fout.close(); // закрываем файл
+		}
+	}
+	catch (std::bad_alloc) {
+		std::cout << "Не удалось выделить память";
+	}
+}
